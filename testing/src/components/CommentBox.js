e2a83a989d3b47c980649ead15a69a81f37122c0
Kevin <kevsalcedo@gmail.com>
2019-04-12 15:36:04 -0500

    boilerplate for components

---
+++ testing/src/components/CommentBox.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+class CommentBox extends React.Component {
+  render() {
+    return <div>CommentBox</div>;
+  }
+}
+export default CommentBox;
---
+++ testing/src/components/CommentList.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class CommentList extends React.Component {
+  render() {
+    return <div>CommentList</div>;
+  }
+}
+
+export default CommentList;
