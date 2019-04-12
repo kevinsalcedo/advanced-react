60afbc7f0d8d2495191f26dbeca9adb6ecfe6cfa
Kevin <kevsalcedo@gmail.com>
2019-04-12 16:45:26 -0500

    added test for comment list

--- testing/src/components/__tests__/App.test.js
+++ testing/src/components/__tests__/App.test.js
@@ -2,6 +2,7 @@ import React from "react";
 import { shallow } from "enzyme";
 import App from "../App";
 import CommentBox from "../CommentBox";
+import CommentList from "../CommentList";
 
 it("shows a comment box", () => {
   // Object received is in a wrapper component
@@ -10,4 +11,10 @@ it("shows a comment box", () => {
   // Get every instance of CommentBox in wrapped
   // Make sure there is only one instance rendered
   expect(wrapped.find(CommentBox).length).toEqual(1);
+});
+
+it("shows a comment list", () => {
+  const wrapped = shallow(<App />);
+
+  expect(wrapped.find(CommentList).length).toEqual(1);
 });
