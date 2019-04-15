1daddff2d3bfc9aec804f6791031efe18bb577ef
Kevin <kevsalcedo@gmail.com>
2019-04-15 16:03:28 -0500

    added logic to be reused in HOC

--- hoc/src/components/CommentBox.js
+++ hoc/src/components/CommentBox.js
@@ -6,6 +6,20 @@ import * as actions from "actions";
 class CommentBox extends React.Component {
   state = { comment: "" };
 
+  componentDidMount() {
+    this.shouldNavigateAway();
+  }
+
+  componentDidUpdate() {
+    this.shouldNavigateAway();
+  }
+
+  shouldNavigateAway() {
+    if (!this.props.auth) {
+      this.props.history.push("/");
+    }
+  }
+
   handleChange = event => {
     this.setState({ comment: event.target.value });
   };
@@ -40,7 +54,11 @@ class CommentBox extends React.Component {
   }
 }
 
+function mapStateToProps(state) {
+  return { auth: state.auth };
+}
+
 export default connect(
-  null,
+  mapStateToProps,
   actions
 )(CommentBox);
