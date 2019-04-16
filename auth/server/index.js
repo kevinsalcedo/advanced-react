2dcab90cb42b2cccad5089502baac94acde25dc6
Kevin <kevsalcedo@gmail.com>
2019-04-16 11:45:35 -0500

    set up boilerplate express app

--- auth/server/index.js
+++ auth/server/index.js
@@ -1,1 +1,22 @@
 // Main starting point of the application
+const express = require("express");
+const http = require("http");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+
+// Create an instance of express
+const app = express();
+
+// App Setup
+// Getting express working how we want
+
+// Server setup
+// Getting our express app to talk to the outside world
+const port = process.env.PORT || 3090;
+
+// createServer - native node library
+// Create an http server that knows how to receive requests
+// Forward these requests to our express app
+const server = http.createServer(app);
+server.listen(port);
+console.log("Server listening on:", port);
