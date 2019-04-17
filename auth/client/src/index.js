6021becb1f2c757f88b6049a052dc169787c89cd
Kevin <kevsalcedo@gmail.com>
2019-04-17 14:08:28 -0500

    scaffolding for signup

--- auth/client/src/index.js
+++ auth/client/src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom";
 import { BrowserRouter, Route } from "react-router-dom";
 import App from "./components/App";
 import Welcome from "./components/Welcome";
+import Signup from "./components/auth/Signup";
 
 ReactDOM.render(
   <BrowserRouter>
     <App>
-      <Route to="/" exact component={Welcome} />
+      <Route path="/" exact component={Welcome} />
+      <Route path="/signup" exact component={Signup} />
     </App>
   </BrowserRouter>,
   document.querySelector("#root")
