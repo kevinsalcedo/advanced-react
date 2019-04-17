73d4c675b00af2c75e114d812628342aca0bff2f
Kevin <kevsalcedo@gmail.com>
2019-04-17 14:16:02 -0500

    redux setup

---
+++ auth/client/src/reducers/authReducer.js
@@ -0,0 +1,12 @@
+// Default state object
+const INITIAL_STATE = {
+  authenticated: "",
+  errorMessage: ""
+};
+
+export default function(state = INITIAL_STATE, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
