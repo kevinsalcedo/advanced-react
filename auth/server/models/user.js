792228844d75f5312313cfffd25e958e2b2c5c20
Kevin <kevsalcedo@gmail.com>
2019-04-17 11:13:08 -0500

    added bcrypt password encryption for user signups

--- auth/server/models/user.js
+++ auth/server/models/user.js
@@ -1,11 +1,36 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const bcrypyt = require("bcrypt-nodejs");
 
 // Define our Model
 const userSchema = new Schema({
   // Any string will be lowercased and then validate for uniqueness
   email: { type: String, unique: true, lowercase: true },
   password: String
+});
+
+// On Save Hook, encrypt password
+// Before saving a user, salt the password
+userSchema.pre("save", function(next) {
+  // Get instance of user model
+  const user = this;
+
+  // Generate 10 round salt
+  bcrypyt.genSalt(10, function(err, salt) {
+    if (err) {
+      return next(err);
+    }
+
+    // Hash salt + password
+    bcrypyt.hash(user.password, salt, null, function(err, hash) {
+      if (err) {
+        return next(err);
+      }
+      // Set password to resulting salted hash
+      user.password = hash;
+      next();
+    });
+  });
 });
 
 // Create the Model class
